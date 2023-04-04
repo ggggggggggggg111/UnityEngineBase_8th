6d86db0c35b5e8ee9f24f4d5ccfec91c6b9b8020
ggggggggggggg111 <95688377+ggggggggggggg111@users.noreply.github.com>
2023-04-04 17:13:55 +0900

    CSharp -Class
    qwer

---
+++ CSharp/CSharpBasic/Inheritance/Goblin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Goblin : Monster, IAttackable
+    {
+        public int AttackPower
+        {
+            get
+            {
+                return _attackPower;
+            }
+        }
+        private int _attackPower;
+
+        public Goblin(int hp, int attackPower)
+            : base(hp) // base 키워드 : 상위타입 참조
+        {
+            this._attackPower = attackPower;
+        }
+
+        public void Attack(IDamageable target)
+        {
+            target.Damage(this, _attackPower);
+        }
+
+        protected override void Breath()
+        {
+            Console.WriteLine("Goblin 이(가) 숨을쉰다.");
+        }
+    }
+}
---
+++ CSharp/CSharpBasic/Inheritance/Resource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Resource : IDamageable
+    {
+        public int Hp
+        {
+            get
+            {
+                return hp;
+            }
+        }
+        protected int hp;
+
+        public void Damage(IAttackable attacker, int value)
+        {
+            if (hp - value < 0)
+            {
+                hp = 0;
+                Console.WriteLine($"{attacker} 가 {this} 를 채집하는데 성공했습니다.");
+            }
+            else
+            {
+                hp -= value;
+                Console.WriteLine($"{attacker} 가 {this} 를 채집중입니다... 남은 체력 : {hp}");
+            }
+        }
+    }
+}
